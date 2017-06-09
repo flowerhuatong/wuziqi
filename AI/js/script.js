10c1ce7aa8b4dbe43d4f7cf158f6b747603f0e61
[flowerhuatong] <[423823255@qq.com]>
2017-06-09 16:50:19 +0800

    feat()AI部分完成，剩余优化！

--- AI/js/script.js
+++ AI/js/script.js
@@ -1,3 +1,5 @@
+//棋局结束状态
+var over = false;
 //落子情况(二维数组)
 var chessBoard = [];
 for (var i = 0; i < 15; i++) {
@@ -6,9 +8,63 @@ for (var i = 0; i < 15; i++) {
 		chessBoard[i][j] = 0;
 	}
 }
+//赢法的数组
+var wins = [];
+for (var i = 0; i < 15; i++) {
+	wins[i] = [];
+	for (var j = 0; j < 15; j++) {
+		wins[i][j] = [];
+	}
+}
 //一开始黑方先落子
 var me = true;
+//赢法种类的索引
+var count = 0;
+//横线赢法
+for (var i = 0; i < 15; i++) {
+	for (var j = 0; j < 11; j++) {
+		for (var k = 0; k < 5; k++) {
+			wins[i][j + k][count] = true;
+		}
+		count++;
+	}
+}
+//竖线赢法
+for (var i = 0; i < 15; i++) {
+	for (var j = 0; j < 11; j++) {
+		for (var k = 0; k < 5; k++) {
+			wins[j + k][i][count] = true;
+		}
+		count++;
+	}
+}
+//斜线赢法
+for (var i = 0; i < 11; i++) {
+	for (var j = 0; j < 11; j++) {
+		for (var k = 0; k < 5; k++) {
+			wins[i + k][j + k][count] = true;
+		}
+		count++;
+	}
+}
+//反斜线赢法
+for (var i = 0; i < 11; i++) {
+	for (var j = 14; j > 3; j--) {
+		for (var k = 0; k < 5; k++) {
+			wins[i + k][j - k][count] = true;
+		}
+		count++;
+	}
+}
+console.log(count);
 
+//赢法的统计数组
+var myWin = [];
+var computerWin = [];
+for (var i = 0; i < count; i++) {
+	myWin[i] = 0;
+	computerWin[i] = 0;
+}
 var chess = document.getElementById('chess');
 //二维绘图 
 var context = chess.getContext('2d');
@@ -44,7 +100,7 @@ var drawChessBoard = function() {
 };
 //落子的位置和颜色
 var oneStep = function(i, j, me) {
-	console.log(i + '+' + j);
+
 	//beginPath() 方法在一个画布中开始子路径的一个新的集合。
 	context.beginPath();
 	context.arc(15 + i * 30, 15 + j * 30, 13, 0, 2 * Math.PI);
@@ -67,6 +123,12 @@ var oneStep = function(i, j, me) {
 
 //落子事件
 chess.onclick = function(e) {
+	if (over) {
+		return;
+	}
+	if (!me) {
+		return;
+	}
 	//点击的时候获得X轴和Y轴的坐标
 	var x = e.offsetX;
 	var y = e.offsetY;
@@ -75,12 +137,107 @@ chess.onclick = function(e) {
 	//判断是否已经落子
 	if (chessBoard[i][j] === 0) {
 		oneStep(i, j, me);
-		//黑棋为1，白棋为2
-		if (me) {
-			chessBoard[i][j] = 1;
-		} else {
-			chessBoard[i][j] = 2;
+		//黑棋为1
+		chessBoard[i][j] = 1;
+		for (var k = 0; k < count; k++) {
+			if (wins[i][j][k]) {
+				myWin[k]++;
+				computerWin[k] = 6;
+				if (myWin[k] === 5) {
+					window.alert('你赢了');
+					over = true;
+				}
+			}
 		}
-		me = !me;
+		if (!over) {
+			me = !me;
+			computerAl();
+		}
 	}
+};
+var computerAl = function() {
+	//人脑和电脑的分数二维数组
+	var myScore = [];
+	var computerScore = [];
+	//最高分数
+	var max = 0;
+	//最高分数的坐标
+	var u = 0,
+		v = 0;
+	for (var i = 0; i < 15; i++) {
+		myScore[i] = [];
+		computerScore[i] = [];
+		for (var j = 0; j < 15; j++) {
+			myScore[i][j] = 0;
+			computerScore[i][j] = 0;
+
+		}
+	}
+
+	for (var i = 0; i < 15; i++) {
+		for (var j = 0; j < 15; j++) {
+			if (chessBoard[i][j] === 0) {
+				for (var k = 0; k < count; k++) {
+					if (wins[i][j][k]) {
+						//人脑拦截程度
+						if (myWin[k] == 1) {
+							myScore[i][j] += 200;
+						} else if (myWin[k] == 2) {
+							myScore[i][j] += 400;
+						} else if (myWin[k] == 3) {
+							myScore[i][j] += 2000;
+						} else if (myWin[k] == 4) {
+							myScore[i][j] += 10000;
+						}
+						//电脑必下程度
+						if (computerWin[k] == 1) {
+							computerScore[i][j] += 220;
+						} else if (computerWin[k] == 2) {
+							computerScore[i][j] += 420;
+						} else if (computerWin[k] == 3) {
+							computerScore[i][j] += 2100;
+						} else if (computerWin[k] == 4) {
+							computerScore[i][j] += 20000;
+						}
+					}
+				}
+				if (myScore[i][j] > max) {
+					max = myScore[i][j];
+					u = i;
+					v = j;
+				} else if (computerScore[i][j] == max) {
+					if(computerScore[i][j]>computerScore[u][v]){
+						u = i;
+						v = j;
+					}	
+				}
+
+				if (computerScore[i][j] > max) {
+					max = myScore[i][j];
+					u = i;
+					v = j;
+				} else if (myScore[i][j] == max) {
+					if(myScore[i][j]>myScore[u][v]){
+						u = i;
+						v = j;
+					}
+				}
+			}
+		}
+	}
+	oneStep(u,v,false);
+	chessBoard[u][v]=2;
+	for (var k = 0; k < count; k++) {
+			if (wins[u][v][k]) {
+				computerWin[k]++;
+				myWin[k] = 6;
+				if (computerWin[k] === 5) {
+					window.alert('你输了');
+					over = true;
+				}
+			}
+		}
+		if (!over) {
+			me = !me;
+		}
 };
